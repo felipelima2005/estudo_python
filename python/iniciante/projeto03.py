d329719e9ed5d471c4860141fcca770af285387e
felipelima2005 <felipelimaoliveira2008@gmail.com>
2026-04-15 12:02:41 -0300

    feat: Desafio iniciante 03 concluido

--- python/iniciante/projeto03.py
+++ python/iniciante/projeto03.py
@@ -4,3 +4,9 @@ Desafio nivel 3: Conversor de temperatura
 
 
 print('---Bem-vindo ao seu conversor de temperatura---')
+
+
+c = float(input('Qqual temperatura você gostaria de converter: '))
+f = (c * 9/5) + 32
+
+print(f'A conversão de {c}C para Fahrenheit é de {f}F')
