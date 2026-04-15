16936e1e7c18ec2c0e97ee94bf1714482e83caf1
felipelima2005 <felipelimaoliveira2008@gmail.com>
2026-04-15 19:08:49 -0300

    feat: Desafio Intermediário 02 concluido

--- python/intermediario/projeto02.py
+++ python/intermediario/projeto02.py
@@ -2,3 +2,27 @@
 Desafio nivel 2: Cadastro de alunos
 '''
 
+lista = {}
+contador = 0
+
+
+print('---Bem-vindo a sua ficha de alunos---')
+print('')
+
+lista ['Felipe'] = 8.5
+lista ['Elias'] = 7
+lista ['Milena'] = 6
+
+for nome, nota in lista.items():
+    print(f'{nome} --> {nota}')
+
+media = (sum(lista.values()))/len(lista.keys())
+print(f'Média da turma --> {media:.2f}')
+
+aprovados = []
+
+for nome, nota in lista.items():
+    if nota >= 7:
+       aprovados.append(nome)
+
+print(f'Aprovados: {','.join(aprovados)}')
